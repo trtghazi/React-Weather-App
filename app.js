6d0be9038c1560182f4e478f43431be68b701a33
Ghazi Khan <gkhan@torinit.com>
2017-03-03 10:46:52 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
 	if(req.headers['x-forwarded-proto'] === 'http'){
-		next();
-	} else {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
-
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-	console.log('Express is running on port' + port);
+	console.log('Express is running on port' + PORT);
 });
