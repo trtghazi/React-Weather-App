b0205324f9092b3bf0b8fc014ae24e0e07f5fb52
Ghazi Khan <gkhan@torinit.com>
2017-03-03 11:32:26 +0530

    updated author name

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -16,7 +16,7 @@ var Nav = React.createClass({
 			<div className="top-bar">
 				<div className="top-bar-left">
 					<ul className="menu">
-						<li className="menu-text">React Weather App</li>
+						<li className="menu-text">React Weather App - By Ghazi Khan</li>
 						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
 						<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
 						<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
